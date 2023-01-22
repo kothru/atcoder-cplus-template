a43a9828d4787c4ebb2001725611143799577349
kothru <1894683+kothru@users.noreply.github.com>
2023-01-22 21:34:11 +0900

    add

---
+++ basic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  bool is = true;
+  int a = 1;
+  // declare var first
+  int a, b;
+  // input
+  cin >> a >> b;
+  // output
+  cout << a << endl;
+  // forloop
+  int N;
+  for (int n = 1; n < N; ++n)
+  {
+  }
+  // while
+  while (N > 0 && N < 0)
+  {
+  }
+  a /= 10;
+  a = b % 10;
+}
+
+int calc(int n)
+{
+}
